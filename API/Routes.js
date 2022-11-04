36061e155210a739fefd01bb6adb903ef540c28c
supundilshan <supundilshandelpagoda@gmail.com>
2022-11-04 10:36:44 +0530

    feat(API): :art: Connect MySQL

--- API/Routes.js
+++ API/Routes.js
@@ -2,6 +2,22 @@ const express = require('express');
 const appRoute = express.Router();
 const mysql = require('mysql');
 
+// Define the  connection with MySQL DB
+const DB = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "mydb"
+});
+// Connect to MySQL
+DB.connect((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("MySql Connected");
+    }
+});
 
 // Define Routes ======================>>>
 
