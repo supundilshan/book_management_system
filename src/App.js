42bc3be031e0b2d0d7083b1df0b383c1d30b88dd
supundilshan <supundilshandelpagoda@gmail.com>
2022-11-04 14:59:28 +0530

    feat(Front End): :art: Create fornt end routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ViewBooks from './Components/ViewBooks';
+import ViewAuthors from './Components/ViewAuthors';
+import AddBook from './Components/AddBook';
+import AddAuthor from './Components/AddAuthor';
+import ViewBookDetails from './Components/ViewBookDetails';
+import ViewAuthorDetails from './Components/ViewAuthorDetails';
+import UpdateBook from './Components/UpdateBook';
+import UpdateAuthor from './Components/UpdateAuthor';
 
 function App() {
   return (
     <div className="App">
-      <ViewBooks />
+      <BrowserRouter>
+        <Routes>
+          {/* Show relevent details about books and authors */}
+          <Route path='/' element={<ViewBooks />} />
+          <Route path='/authors' element={<ViewAuthors />} />
+
+          {/* Show all details about books and authors after user click*/}
+          <Route path='/book' element={<ViewBookDetails />} />
+          <Route path='/author' element={<ViewAuthorDetails />} />
+
+          {/* Add book and author detail */}
+          <Route path='/addbook' element={<AddBook />} />
+          <Route path='/addauthor' element={<AddAuthor />} />
+
+          {/* Update Book and Author details */}
+          <Route path='/update-book' element={<UpdateBook />} />
+          <Route path='/update-author' element={<UpdateAuthor />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/ViewBooks.jsx
+++ src/Components/ViewBooks.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const ViewBooks = () => {
+    const [dbdata, getDbdata] = useState([]);
+
+    // Get Data fromDatabase
+    useEffect(() => {
+        axios.get('http://localhost:3001/books')
+            .then((res) => {
+                getDbdata(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    const ViewBook = () => {
+
+    }
+
     return (
         <div>
-            view books
+            <h2>Book List</h2>
+            <ul>
+                {dbdata.map((dbdata, key) => {
+                    return <li className='data-table'>
+                        {dbdata.Name}
+                        <button onClick={() => ViewBook(dbdata.ID)}>View Book Data</button>
+                    </li>
+                })}
+            </ul>
+            {/* <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {dbdata.map((dbdata, key) => {
+                        return <tr className='data-table'>
+                            <td>{dbdata.Name}</td>
+                            <td> <button onClick={() => deleteData(dbdata.ID)}>Delete</button> </td>
+                            <td> <button onClick={() => updateData(dbdata.ID)}>Update</button> </td>
+                        </tr>
+                    })}
+                </tbody>
+            </table> */}
         </div>
     );
 };
