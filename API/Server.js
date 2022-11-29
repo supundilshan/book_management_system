c6493db0b30cf0c8054f46d3f112e6437e3fb722
supundilshan <supundilshandelpagoda@gmail.com>
2022-11-29 21:13:23 +0530

    Routes.js moved to RouteControllers folder

--- API/Server.js
+++ API/Server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const AppRoute = require('./Routes');
+const AppRoute = require('./RouteModels/Routes');
 
 const app = express();
 
